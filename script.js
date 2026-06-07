7c6682ec2c5ee60103de8b246602e80bda737501
ahmedrozto <ahmedrozto@gmail.com>
2026-06-07 21:58:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-let slides = document.querySelectorAll(".slide");
-let index = 0;
+fetch("menu.json")
+.then(res=>res.json())
+.then(data=>{
 
-setInterval(() => {
+const container=document.getElementById("menuContainer");
 
-    slides[index].classList.remove("active");
+function render(title,items){
 
-    index = (index + 1) % slides.length;
+let html=`<div class="category"><h3>${title}</h3>`;
 
-    slides[index].classList.add("active");
+items.forEach(i=>{
 
-}, 3000);
+html+=`
+<div class="item">
+
+<div class="item-left">
+
+${i.image ? `<img src="${i.image}">` : ""}
+
+<span>${i.name}</span>
+
+</div>
+
+<span>${i.price ? i.price + " جنيه" : ""}</span>
+
+</div>
+`;
+});
+
+html+=`</div>`;
+
+container.innerHTML+=html;
+}
+
+render("🐟 الأسماك",data.fish);
+render("🍤 الأطباق الجانبية",data.sides);
+render("🥗 السلطات",data.salads);
+render("🥤 المشروبات",data.drinks);
+
+});
+
+/* SLIDER */
+let slides=document.querySelectorAll(".slide");
+let i=0;
+
+setInterval(()=>{
+slides[i].classList.remove("active");
+i=(i+1)%slides.length;
+slides[i].classList.add("active");
+},3500);
+
+/* SCROLL ANIMATION */
+window.addEventListener("scroll",()=>{
+document.querySelectorAll(".title").forEach(el=>{
+let pos=el.getBoundingClientRect().top;
+if(pos<600) el.classList.add("show");
+});
+});
